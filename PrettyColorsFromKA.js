2bcbd587ccbff54b33869412fba27d0902b9df7d
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-15 17:50:15 -0400

    object fit contain

---
+++ PrettyColorsFromKA.js
@@ -0,0 +1,178 @@
+background(255, 255, 255);
+
+
+
+  
+    
+    
+
+     
+     
+
+draw= function() {
+    //Starting places
+var RedX = random(0, 400);
+var RedY = random(0, 400);
+var GreenX = random(0, 400);
+var GreenY = random(0, 400);
+var BlueX = random(0, 400);
+var BlueY = random(0, 400);
+//red logic
+var direction = random(0, 8);
+    if (direction === 1){ 
+        RedY++;}
+    if (direction === 2){ 
+        RedX++;
+        RedY++;}
+     if (direction === 3){ 
+        RedX++;}
+     if (direction === 4){ 
+        RedX++;
+        RedY--;}
+   if (direction === 5){ 
+        RedY--;}
+    if (direction === 6){ 
+        RedX--;
+        RedY--;}
+    if (direction === 7){ 
+        RedX--;
+    if (direction ===8){
+        RedX--;
+        RedY++;}
+        
+        
+        
+    }
+        
+        
+        
+        
+//Painters
+//Red
+fill(255, 0, 0);
+stroke(255, 0, 0);
+ellipse(RedX, RedY, 1, 1);
+//Green
+fill(0, 255, 0);
+stroke(0, 255, 0);
+ellipse(GreenX, GreenY, 1, 1);
+//Blue
+fill(0, 0, 255);
+stroke(0, 0, 255);
+ellipse(BlueX, BlueY, 1, 1); 
+
+
+
+    //Starting places 2
+var RedX = random(0, 400);
+var RedY = random(0, 400);
+var GreenX = random(0, 400);
+var GreenY = random(0, 400);
+var BlueX = random(0, 400);
+var BlueY = random(0, 400);
+//red logic
+var direction = random(0, 8);
+    if (direction === 1){ 
+        RedY++;}
+    if (direction === 2){ 
+        RedX++;
+        RedY++;}
+     if (direction === 3){ 
+        RedX++;}
+     if (direction === 4){ 
+        RedX++;
+        RedY--;}
+   if (direction === 5){ 
+        RedY--;}
+    if (direction === 6){ 
+        RedX--;
+        RedY--;}
+    if (direction === 7){ 
+        RedX--;
+    if (direction ===8){
+        RedX--;
+        RedY++;}
+        
+        
+        
+    }
+        
+        
+        
+        
+//Painters
+//Red
+fill(255, 0, 0);
+stroke(255, 0, 0);
+ellipse(RedX, RedY, 1, 1);
+//Green
+fill(0, 255, 0);
+stroke(0, 255, 0);
+ellipse(GreenX, GreenY, 1, 1);
+//Blue
+fill(0, 0, 255);
+stroke(0, 0, 255);
+ellipse(BlueX, BlueY, 1, 1); 
+
+
+    //Starting places3
+var RedX = random(0, 400);
+var RedY = random(0, 400);
+var GreenX = random(0, 400);
+var GreenY = random(0, 400);
+var BlueX = random(0, 400);
+var BlueY = random(0, 400);
+//red logic
+var direction = random(0, 8);
+    if (direction === 1){ 
+        RedY++;}
+    if (direction === 2){ 
+        RedX++;
+        RedY++;}
+     if (direction === 3){ 
+        RedX++;}
+     if (direction === 4){ 
+        RedX++;
+        RedY--;}
+   if (direction === 5){ 
+        RedY--;}
+    if (direction === 6){ 
+        RedX--;
+        RedY--;}
+    if (direction === 7){ 
+        RedX--;
+    if (direction ===8){
+        RedX--;
+        RedY++;}
+        
+        
+        
+    }
+        
+        
+        
+        
+//Painters
+//Red
+fill(255, 0, 0);
+stroke(255, 0, 0);
+ellipse(RedX, RedY, 1, 1);
+//Green
+fill(0, 255, 0);
+stroke(0, 255, 0);
+ellipse(GreenX, GreenY, 1, 1);
+//Blue
+fill(0, 0, 255);
+stroke(0, 0, 255);
+ellipse(BlueX, BlueY, 1, 1); 
+
+
+
+
+
+
+};
+    
+
+
+
