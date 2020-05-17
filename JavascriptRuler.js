b78500928554ccd8753604221e6a207b143c9d90
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-17 00:32:57 -0400

    Ruler?

--- JavascriptRuler.js
+++ JavascriptRuler.js
@@ -2,7 +2,7 @@
 function MakeStuffHappen(){
 document.getElementById("MakeStuffHappen").style.display='block';
 
-
+document.write("Hello, World!");
 print("Hello World!");
 fill(0,0,0);
 rect(50,50,50,50);
