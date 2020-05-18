a7d119722796a705ffdeb66b856e380e4d0263d8
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-18 17:24:37 -0400

    Can we change the square size from 50 to ten?

--- JavascriptRuler.js
+++ JavascriptRuler.js
@@ -14,7 +14,7 @@ var c=canvas.getContext("2d");
 //ctx.beginPath();
 //ctx.rect(150, 150, 50, 50);
 //ctx.fillStyle=rgb(0,0,255);
-var SquareWidth=50;
+var SquareWidth=5;
 var InitialPos=2*SquareWidth;
 
 c.fillRect(InitialPos, InitialPos, SquareWidth, SquareWidth);
