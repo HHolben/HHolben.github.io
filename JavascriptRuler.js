25dc93eac8b37692a51177214c97f10f8e5553e9
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-18 17:35:52 -0400

    now changing it to 1?

--- JavascriptRuler.js
+++ JavascriptRuler.js
@@ -14,7 +14,7 @@ var c=canvas.getContext("2d");
 //ctx.beginPath();
 //ctx.rect(150, 150, 50, 50);
 //ctx.fillStyle=rgb(0,0,255);
-var SquareWidth=5;
+var SquareWidth=1;
 var InitialPos=2*SquareWidth;
 
 c.fillRect(InitialPos, InitialPos, SquareWidth, SquareWidth);
