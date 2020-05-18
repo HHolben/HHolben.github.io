32ca0e01d4b4bb9cc2764bcdeca8f04235a8a3eb
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-18 01:19:13 -0400

    doing a tutorial

--- JavascriptRuler.js
+++ JavascriptRuler.js
@@ -9,9 +9,12 @@ rect(50,50,50,50);
 
 window.alert("Welcome to the party");
    
-var canvas=document.querySelector("  Canvas");
-var ctx=canvas.getContext("2d");
+var canvas=document.querySelector("Canvas");
+var c=canvas.getContext("2d");
 //ctx.beginPath();
 //ctx.rect(150, 150, 50, 50);
-ctx.fillStyle=rgb(0,0,255);
-ctx.fillRect(0, 0, 100, 100);
+//ctx.fillStyle=rgb(0,0,255);
+C.fillRect(100, 100, 100, 100);
+
+
+ 
