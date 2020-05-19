78fb9edb7587f7610299e8b06e00dee970a9ee67
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-19 00:22:25 -0400

    I want colors!!!!

--- JavascriptRuler.js
+++ JavascriptRuler.js
@@ -47,7 +47,7 @@ var Red=Math.floor(Math.random()*255);
 var Blue=Math.floor(Math.random()*255);
 var Green=Math.floor(Math.random()*255);
 
-c.fillStyle="rgb(red,green,blue)";
+c.fillStyle="rgb(Red,Green,Blue)";
 c.fillRect(SpaceBetween*IOne+InitialPos, SpaceBetween*ITwo+InitialPos, SquareWidth, SquareWidth);
 c.fillRect(SpaceBetween*IOne+InitialPos/2, SpaceBetween*ITwo+InitialPos*1.5, 3*SquareWidth, SquareWidth);
 c.fillRect(SpaceBetween*IOne+InitialPos, SpaceBetween*ITwo+InitialPos*2, SquareWidth, SquareWidth);
