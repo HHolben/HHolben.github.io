ae86111a94c51b17ae85759ea237f0f2f67685ce
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-19 00:19:45 -0400

    hmmmmm

--- JavascriptRuler.js
+++ JavascriptRuler.js
@@ -11,11 +11,13 @@ window.alert("Welcome to the party");
    
 var canvas=document.querySelector("Canvas");
 var c=canvas.getContext("2d");
-//ctx.beginPath();
-//ctx.rect(150, 150, 50, 50);
-//ctx.fillStyle=rgb(0,0,255);
+
+
+
 var SquareWidth=3;
 var InitialPos=2*SquareWidth;
+
+
 
 
 
