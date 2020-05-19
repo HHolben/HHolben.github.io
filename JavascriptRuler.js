d003f512d555d9df88a357fd25b8f3c1136f4a06
HHolben <47185193+HHolben@users.noreply.github.com>
2020-05-18 22:33:25 -0400

    hopefully this doesn't break it

--- JavascriptRuler.js
+++ JavascriptRuler.js
@@ -14,9 +14,26 @@ var c=canvas.getContext("2d");
 //ctx.beginPath();
 //ctx.rect(150, 150, 50, 50);
 //ctx.fillStyle=rgb(0,0,255);
-var SquareWidth=1;
+var SquareWidth=3;
 var InitialPos=2*SquareWidth;
+
+
 
 c.fillRect(InitialPos, InitialPos, SquareWidth, SquareWidth);
 c.fillRect(InitialPos/2, InitialPos*1.5, 3*SquareWidth, SquareWidth);
 c.fillRect(InitialPos, InitialPos*2, SquareWidth, SquareWidth);
+
+var PlusPerRow=25;
+var IOne=1;
+var SpaceBetween=15*SquareWidth;
+
+while(IOne<PlusPerRow)
+{
+c.fillRect(SpaceBetween*IOne+InitialPos, InitialPos, SquareWidth, SquareWidth);
+c.fillRect(SpaceBetween*IOne+InitialPos/2, InitialPos*1.5, 3*SquareWidth, SquareWidth);
+c.fillRect(SpaceBetween*IOne+InitialPos, InitialPos*2, SquareWidth, SquareWidth);
+
+
+    IOne++;
+
+}
